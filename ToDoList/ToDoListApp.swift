4f51a35d86518e9854b56187ba70213f29ec3e8d
indramhaarta <indramhrt@gmail.com>
2024-03-13 14:43:02 +0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by I Made Indra Mahaarta on 13/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
